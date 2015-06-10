c30fdf724dfcfd47cd4f3dd8d4b301e423a7c10b
Artem Pyankov <artpyankov@gmail.com>
2015-06-10 23:49:54 +0500

    Implement adding answers with AJAX

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -22,6 +22,10 @@ RSpec.describe QuestionsController, type: :controller do
       expect(assigns(:question)).to eq question
     end
 
+    it 'assigns new answer to the question' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
     it { should render_template :show }
   end
 
