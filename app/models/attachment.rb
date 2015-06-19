1b7d56b303072c135de60bfef30c9172435c7e4e
Artem Pyankov <artpyankov@gmail.com>
2015-06-20 01:07:27 +0500

    Move view logic from model to js view

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,9 +1,6 @@
 class Attachment < ActiveRecord::Base
   belongs_to :attachable, polymorphic: true
 
-  delegate :attachments, to: :attachable
-  alias_method :other_attachments, :attachments
-
   mount_uploader :file, FileUploader
   validates :file, presence: true
 end
