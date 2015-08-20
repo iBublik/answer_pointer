f4c1bf8e69284462808ba4aafb4f5b4c08fd808a
Artem Pyankov <artpyankov@gmail.com>
2015-08-20 18:09:48 +0500

    Fix caching of manage links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,9 @@ module ApplicationHelper
     max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s, :number)
     "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
   end
+
+  def cache_key_for_user(model, description)
+    user_id = user_signed_in? ? current_user.id : 0
+    "#{model.to_s.pluralize}/#{description}-#{model.id}-for-user-#{user_id}"
+  end
 end
